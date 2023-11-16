c6f2921255c4188e55d9a6ab5bd1c6f1ea89beaf
Shirin Ebadi <shirinshirin00@gmail.com>
2023-11-17 01:01:09 +0700

    second commit

--- admin/index.php
+++ admin/index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Tables</title>
+    <title>Table Admin</title>
 
     <!-- Custom fonts for this template -->
     <link href="../assets/css/pindah/all.min.css" rel="stylesheet" type="text/css">
